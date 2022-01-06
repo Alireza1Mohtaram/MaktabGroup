89d8f7ad794a90e847cfd72be5114c44520d0615
Alireza Mohtaram <Alireza.Pro8106@gmail.com>
2022-01-06 16:20:27 +0330

    Adding Kotlin file

---
+++ Account.kt
@@ -0,0 +1,19 @@
+import java.lang.Exception
+
+class Account(accountNumber:String, balance :Double, branch :Branch ) {
+
+
+
+    var accountNumber:String=accountNumber
+    set(value) {
+        if (value.trim().isEmpty()) throw Exception()
+
+        field = value
+    }
+    var balance :Double =balance
+    set(value) {
+        if (value < 0) field = 0.0
+        field = value
+    }
+    var branch :Branch = branch
+}
